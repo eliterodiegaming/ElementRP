33ed5aaddc62f7a8e7c54689b7468aef1648cc01
eliterodiegaming <101967343+eliterodiegaming@users.noreply.github.com>
2022-03-20 15:44:24 -0400

    Fixed Cruise Control Bugs

--- [scripts]/cruisecontrol/cl.lua
+++ [scripts]/cruisecontrol/cl.lua
@@ -7,10 +7,10 @@ function showText(text, x, y, r, g, b)
     SetTextProportional(0)
     SetTextScale(1.9,1.9)
     SetTextOutline()
-    SetTextColour(r,g,b,255)
+    SetTextColour(r, g, b, 255)
     SetTextEntry("STRING")
     AddTextComponentString(text)
-    DrawText(x,y)
+    DrawText(x, y)
 end
 
 function notify(message)
@@ -22,25 +22,16 @@ end
 Citizen.CreateThread(function()
     while true do 
         Citizen.Wait(0)
+        local speed = (GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*mph)
         if (IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
-            -- Speedometer
-            local speed = (GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*mph)
-            if speed < 1 then
-                showText("0", 0.02, 0.70, 155, 155, 155)
-            elseif speed >= 80 then
-                showText(math.floor(speed), 0.02, 0.70, 255, 0 , 0)
-            else
-                showText(math.floor(speed), 0.02, 0.70. 255, 255, 255)
-            end
-
             -- Cruise Control
             if IsControlJustPressed(1, 246) then
                 if cruise == false then
                     cruise = true
                     local currentSpeed = (GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false)))
-                    SetVehicleMinSpeed = (GetVehiclePedIsIn(GetPlayerPed(-1), false), currentSpeed)
+                    SetVehicleMinSpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false), currentSpeed)
                     notify("Cruise Control Speed Set:" .. currentSpeed*mph .. "mph")
-                elseif cruise == true
+                elseif cruise == true then
                     cruise = false
                     SetVehicleMinSpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false), 0.0)
                     notify("Cruise Control Disabled")
